e4d26d2892e4b88828dac7511be45449d914588f
TBHGodPro <tarun.bhaskar@outlook.com>
2024-07-08 17:12:48 -0500

    npm ready

--- scripts/copyPackets.mjs
+++ scripts/copyPackets.mjs
@@ -1,10 +1,5 @@
-import { copyFile } from "fs/promises";
-import { join } from "path";
-import { cwd } from "process";
+import { copyFile } from 'fs/promises';
+import { join } from 'path';
+import { cwd } from 'process';
 
-await Promise.all([
-    copyFile(join(cwd(), './special/packets.js'), join(cwd(), './dist/packets/packets.js')),
-    copyFile(join(cwd(), './special/packets.d.ts'), join(cwd(), './dist/packets/packets.d.ts')),
-    copyFile(join(cwd(), './special/extra.js'), join(cwd(), './dist/packets/extra.js')),
-    copyFile(join(cwd(), './special/extra.d.ts'), join(cwd(), './dist/packets/extra.d.ts'))
-])
+await Promise.all([copyFile(join(cwd(), './special/packets.js'), join(cwd(), './dist/packets/packets.js')), copyFile(join(cwd(), './special/packets.d.ts'), join(cwd(), './dist/packets/packets.d.ts')), copyFile(join(cwd(), './special/packets.full.mjs'), join(cwd(), './dist/packets/packets.full.mjs')), copyFile(join(cwd(), './special/extra.js'), join(cwd(), './dist/packets/extra.js')), copyFile(join(cwd(), './special/extra.d.ts'), join(cwd(), './dist/packets/extra.d.ts')), copyFile(join(cwd(), './special/extra.full.mjs'), join(cwd(), './dist/packets/extra.full.mjs'))]);
